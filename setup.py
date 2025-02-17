dff22bc5d7787ad3881dbac62d8b449876823797
jhuang <jhuang@mbp>
2025-02-16 21:31:23 -0500

    Moved files to Cubiks_Rube folder, add setup.py and init.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Cubiks_Rube',  # Replace with your own program name
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pygame',
+        'numpy',
+        'tkinter'
+        # Add any other dependencies your program needs
+    ],
+    entry_points={
+        'console_scripts': [
+            'my_program=my_program.main:main',  # Replace with your main entry point
+        ],
+    },
+    author='yeeto38',
+    author_email='',
+    description='Rubik\'s Cube Utility',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yeeto38/cubiksrube',  # Replace with your project's URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
